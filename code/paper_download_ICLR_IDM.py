c742c1bd51f059394cd0b294949274b30a54e87b
silenceagle <czliuzechao@outlook.com>
2021-01-19 22:10:12 +0800

    add support for ICLR 2021
    add support for ICLR 2021
    update ICLR code for calling IDM or Thunder in 'lib' 
    add function 'download_iclr_paper_given_html_file'

--- code/paper_download_ICLR_IDM.py
+++ code/paper_download_ICLR_IDM.py
@@ -2,6 +2,7 @@
 
 import time
 from tqdm import tqdm
+import subprocess
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
