21bf0fe63fc61007f9dfb65024f6d5c12d916935
silenceagle <czliuzechao@outlook.com>
2021-03-15 10:15:21 +0800

    add support for AAAI 2020
    1. add support for AAAI 2020
    2. update AAAI to support lib.IDM

--- code/paper_download_AAAI_IDM.py
+++ code/paper_download_AAAI_IDM.py
@@ -7,7 +7,7 @@ import bs4
 from bs4 import BeautifulSoup
 import pickle
 # from PyPDF2 import PdfFileMerger
-from PyPDF3 import PdfFileMerger
+# from PyPDF3 import PdfFileMerger
 import zipfile
 import os
 import shutil
@@ -15,6 +15,8 @@ from tqdm import tqdm
 import subprocess
 from slugify import slugify
 import csv
+import lib.IDM as IDM
+import lib.thunder as Thunder
 
 
 def save_csv(year):
@@ -160,22 +162,80 @@ def save_csv(year):
     return paper_index
 
 
-def download_from_csv(year, save_dir, time_step_in_seconds=5, total_paper_number=None):
+def save_csv_given_urls(urls, csv_filename='AAAI_tmp.csv'):
+    """
+    write IJCAI papers' urls in one csv file
+    :param urls: str, the urls of paper website, such as 'https://www.aaai.org/Library/AAAI/aaai20contents-issue01.php'
+    :param csv_filename: str, csv file's name, default to 'AAAI_tmp.csv'
+    :return: peper_index: int, the total number of papers
+    """
+    with open(f'..\\csv\\{csv_filename}', 'w', newline='') as csvfile:
+        fieldnames = ['title', 'link', 'group']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        content = urlopen(urls).read()
+        paper_index = 0
+        soup = BeautifulSoup(content, 'html5lib')
+        error_log = []
+        pbar = tqdm(soup.find('div', {'class': 'content'}).find_all(['a', 'p']))
+        this_group = ''
+        for paper in pbar:
+            if 'a' == paper.name:
+                if paper.get('name') is not None:
+                    this_group = slugify(paper.text)
+            else:
+                paper_index += 1
+                all_a = paper.find_all('a')
+                title = slugify(all_a[0].text)
+                pbar.set_description(f'downloading paper {paper_index}: {title}')
+                is_get_link = False
+                if 'pdf' == slugify(all_a[1].text):
+                    link = urllib.parse.urljoin(urls, all_a[1].get('href'))
+                    is_get_link = True
+                if is_get_link:
+                    paper_dict = {'title': title,
+                                  'link': link,
+                                  'group': this_group}
+                else:
+                    paper_dict = {'title': title,
+                                  'link': 'error',
+                                  'group': this_group}
+                    print(f'get link for {title}_{year} failed!')
+                    error_log.apend(title, 'no link')
+                writer.writerow(paper_dict)
+        #  write error log
+        print('write error log')
+        with open('..\\log\\download_err_log.txt', 'w') as f:
+            for log in tqdm(error_log):
+                for e in log:
+                    if e is not None:
+                        f.write(e)
+                    else:
+                        f.write('None')
+                    f.write('\n')
+
+                f.write('\n')
+
+    return paper_index if paper_index is not None else None
+
+
+def download_from_csv(
+        year, save_dir, time_step_in_seconds=5, total_paper_number=None, csv_filename=None, downloader='IDM'):
     """
     download all AAAI paper given year
     :param year: int, AAAI year, such 2019
     :param save_dir: str, paper and supplement material's save path
     :param time_step_in_seconds: int, the interval time between two downlaod request in seconds
     :param total_paper_number: int, the total number of papers that is going to download
+    :param csv_filename: None or str, the csv file's name, None means to use default setting
+    :param downloader: str, the downloader to download, could be 'IDM' or 'Thunder', default to 'IDM'
     :return: True
     """
-    # use IDM to download everything
-    idm_path = '''"C:\Program Files (x86)\Internet Download Manager\IDMan.exe"'''  # should replace by the local IDM path
-    basic_command = [idm_path, '/d', 'xxxx', '/p', os.getcwd(), '/f', 'xxxx', '/n']
 
     error_log = []
     postfix = f'AAAI_{year}'
-    with open(f'..\\csv\\AAAI_{year}.csv', newline='') as csvfile:
+    with open(f'..\\csv\\AAAI_{year}.csv' if csv_filename is None else f'..\\csv\\{csv_filename}', newline='') as csvfile:
         myreader = csv.DictReader(csvfile, delimiter=',')
         pbar = tqdm(myreader)
         i = 0
@@ -199,15 +259,22 @@ def download_from_csv(year, save_dir, time_step_in_seconds=5, total_paper_number
                 error_log.append((title, 'no link'))
             elif '' != this_paper['link']:
                 try:
-                    # download paper with IDM
-                    basic_command[2] = this_paper['link']
-                    basic_command[6] = this_paper_main_path
-                    p = subprocess.Popen(' '.join(basic_command))
-                    p.wait()
-                    time.sleep(time_step_in_seconds)
-                    # while True:
-                    #     if os.path.exists(this_paper_main_path):
-                    #         break
+                    if 'IDM' == downloader:
+                        IDM.download(
+                            urls=this_paper['link'],
+                            save_path=os.path.join(os.getcwd(), this_paper_main_path),
+                            time_sleep_in_seconds=time_step_in_seconds
+                        )
+                    elif 'Thunder' == downloader:
+                        Thunder.download(
+                            urls=this_paper['link'],
+                            save_path=os.path.join(os.getcwd(), this_paper_main_path),
+                            time_sleep_in_seconds=time_step_in_seconds
+                        )
+                    else:
+                        raise ValueError(
+                            f'''ERROR: Unsupported downloader: {downloader}, we currently only support'''
+                            f''' "IDM" or "Thunder" ''')
                 except Exception as e:
                     # error_flag = True
                     print('Error: ' + title + ' - ' + str(e))
@@ -234,13 +301,25 @@ if __name__ == '__main__':
     # download_from_csv(year, save_dir=f'..\\AAAI_{year}',
     #                   time_step_in_seconds=2,
     #                   total_paper_number=total_paper_number)
-    for year in range(2012, 2018, 2):
-        print(year)
-        total_paper_number = None
-        # total_paper_number = save_csv(year)
-        download_from_csv(year, save_dir=f'..\\AAAI_{year}',
-                          time_step_in_seconds=10,
-                          total_paper_number=total_paper_number)
-        time.sleep(2)
+    # for year in range(2012, 2018, 2):
+    #     print(year)
+    #     total_paper_number = None
+    #     # total_paper_number = save_csv(year)
+    #     download_from_csv(year, save_dir=f'..\\AAAI_{year}',
+    #                       time_step_in_seconds=10,
+    #                       total_paper_number=total_paper_number)
+    #     time.sleep(2)
+    for i in range(1, 11):
+        total_paper_number = save_csv_given_urls(
+            urls=f'https://www.aaai.org/Library/AAAI/aaai20contents-issue{i:0>2}.php',
+            csv_filename='AAAI_tmp.csv'
+        )
+        # total_paper_number = 156
+        download_from_csv(
+            year=2020,
+            csv_filename='AAAI_tmp.csv',
+            save_dir=f'..\\..\\AAAI_2020',
+            time_step_in_seconds=5,
+            total_paper_number=total_paper_number)
 
     pass
