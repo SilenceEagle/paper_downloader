e5bb3b4318738df6902b8b3eec548b9716a2040a
silenceagle <czliuzechao@outlook.com>
2021-06-18 13:41:11 +0800

    rewrite to call lib.pmlr
    rewrite to call the reusable lib.pmlr

--- code/paper_downloader_COLT.py
+++ code/paper_downloader_COLT.py
@@ -1,24 +1,16 @@
 """paper_downloader_COLT.py"""
-
-from urllib.request import urlopen
-import time
-from bs4 import BeautifulSoup
-import pickle
-# from PyPDF2 import PdfFileMerger
-from PyPDF3 import PdfFileMerger
-import zipfile
-import os
-import shutil
-from tqdm import tqdm
-import subprocess
-from slugify import slugify
+import lib.pmlr as pmlr
 
 
-def download_paper_IDM(year, save_dir):
+def download_paper(year, save_dir, is_download_supplement=True, time_step_in_seconds=5, downloader='IDM'):
     """
-    download all COLT papers given year, restore in save_dir
-    :param year: int, COLT year, such 2019
+    download all COLT paper and supplement files given year, restore in save_dir/main_paper and save_dir/supplement
+    respectively
+    :param year: int, COLT year, such as 2019
     :param save_dir: str, paper and supplement material's save path
+    :param is_download_supplement: bool, True for downloading supplemental material
+    :param time_step_in_seconds: int, the interval time between two downlaod request in seconds
+    :param downloader: str, the downloader to download, could be 'IDM' or 'Thunder', default to 'IDM'
     :return: True
     """
     COLT_year_dict = {2020: 125,
@@ -32,93 +24,29 @@ def download_paper_IDM(year, save_dir):
                       2012: 23,
                       2011: 19
                       }
-    if year >= 2011 and year <= 2020:
-        init_url = f'http://proceedings.mlr.press/v{COLT_year_dict[year]}/'
+    if year in COLT_year_dict.keys():
+        volume = f'v{COLT_year_dict[year]}'
     else:
         raise ValueError('''the given year's url is unknown !''')
-    # init_url = f'http://papers.nips.cc/book/advances-in-neural-information-processing-systems-{year-1987}-{year}'
-    os.makedirs(save_dir, exist_ok=True)
-    # use IDM to download everything
-    idm_path = '''"C:\Program Files (x86)\Internet Download Manager\IDMan.exe"'''  # should replace by the local IDM path
-    basic_command = [idm_path, '/d', 'xxxx', '/p', os.getcwd(), '/f', 'xxxx', '/n']  # silent /n
-    # create current dict
-    title_list = []
-    # paper_dict = dict()
-
     postfix = f'COLT_{year}'
-    if os.path.exists(f'..\\urls\\init_url_COLT_{year}.dat'):
-        with open(f'..\\urls\\init_url_COLT_{year}.dat', 'rb') as f:
-            content = pickle.load(f)
-    else:
-        content = urlopen(init_url).read()
-        with open(f'..\\urls\\init_url_COLT_{year}.dat', 'wb') as f:
-            pickle.dump(content, f)
-    soup = BeautifulSoup(content, 'html.parser')
-    paper_list = soup.find_all('div', {'class': 'paper'})
-    error_log = []
-    # num_download = 5 # number of papers to download
-    num_download = len(paper_list)
-    for paper in tqdm(zip(paper_list, range(num_download))):
-        # get title
-        print('\n')
-        this_paper = paper[0]
-        title = slugify(this_paper.find_all('p', {'class': 'title'})[0].text)
-        try:
-            print('Downloading paper {}/{}: {}'.format(paper[1]+1, num_download, title))
-        except:
-            print(title.encode('utf8'))
-        title_list.append(title)
 
-        this_paper_main_path = os.path.join(save_dir, f'{title}_{postfix}.pdf')
-        if os.path.exists(this_paper_main_path):
-            continue
-
-        # get abstract page url
-        links = this_paper.find_all('p', {'class': 'links'})[0].find_all('a')
-        main_link = None
-        for link in links:
-            if 'Download PDF' == link.text:
-                main_link = link.get('href')
-
-        # try 1 time
-        # error_flag = False
-        for d_iter in range(1):
-            try:
-                # download paper with IDM
-                if not os.path.exists(this_paper_main_path) and main_link is not None:
-                    basic_command[2] = main_link
-                    basic_command[6] = this_paper_main_path
-                    p = subprocess.Popen(' '.join(basic_command))
-                    p.wait()
-                    time.sleep(5)
-                    # while True:
-                    #     if os.path.exists(this_paper_main_path):
-                    #         break
-            except Exception as e:
-                # error_flag = True
-                print('Error: ' + title + ' - ' + str(e))
-                error_log.append((title, main_link, 'main paper download error', str(e)))
-
-    # store the results
-    # 1. store in the pickle file
-    # with open(f'{postfix}_pre.dat', 'wb') as f:
-    #     pickle.dump(paper_dict, f)
-
-    # 2. write error log
-    print('write error log')
-    with open('..\\log\\download_err_log.txt', 'w') as f:
-        for log in tqdm(error_log):
-            for e in log:
-                if e is not None:
-                    f.write(e)
-                else:
-                    f.write('None')
-                f.write('\n')
-
-            f.write('\n')
+    pmlr.download_paper_given_volume(
+        volume=volume,
+        save_dir=save_dir,
+        postfix=postfix,
+        is_download_supplement=is_download_supplement,
+        time_step_in_seconds=time_step_in_seconds,
+        downloader=downloader
+    )
 
 
 if __name__ == '__main__':
-    year = 2020
-    download_paper_IDM(year, f'..\\COLT_{year}')
+    year = 2011
+    download_paper(
+        year,
+        rf'D:\COLT_{year}',
+        is_download_supplement=True,
+        time_step_in_seconds=5,
+        downloader='IDM'
+    )
     pass
