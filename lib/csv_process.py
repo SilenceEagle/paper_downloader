b2f5ea15605e9a25065ee77b6eae911cd821e1af
silenceagle <czliuzechao@outlook.com>
2021-06-27 16:03:15 +0800

    Add files via upload

--- lib/csv_process.py
+++ lib/csv_process.py
@@ -30,9 +30,10 @@ def download_from_csv(
     if not os.path.exists(csv_file_path):
         raise ValueError(f'ERROR: file not found in {csv_file_path}!!!')
     main_save_path = os.path.join(save_dir, 'main_paper')
-    supplement_save_path = os.path.join(save_dir, 'supplement')
     os.makedirs(main_save_path, exist_ok=True)
-    os.makedirs(supplement_save_path, exist_ok=True)
+    if is_download_supplement:
+        supplement_save_path = os.path.join(save_dir, 'supplement')
+        os.makedirs(supplement_save_path, exist_ok=True)
 
     error_log = []
     with open(csv_file_path, newline='') as csvfile:
@@ -40,10 +41,10 @@ def download_from_csv(
         pbar = tqdm(myreader)
         i = 0
         for this_paper in pbar:
-            is_workshops = ('group' in this_paper)
+            is_grouped = ('group' in this_paper)
             i += 1
             # get title
-            if is_workshops:
+            if is_grouped:
                 group = slugify(this_paper['group'])
             title = slugify(this_paper['title'])
             if total_paper_number is not None:
@@ -53,10 +54,10 @@ def download_from_csv(
                 pbar.set_description(f'Downloading paper {i}')
 
             this_paper_main_path = os.path.join(main_save_path, f'{title}_{postfix}.pdf')
-            if is_workshops:
+            if is_grouped:
                 this_paper_main_path = os.path.join(main_save_path, group, f'{title}_{postfix}.pdf')
             this_paper_supp_path_no_ext = os.path.join(supplement_save_path, f'{title}_{postfix}_supp.')
-            if is_workshops:
+            if is_grouped:
                 this_paper_supp_path_no_ext = os.path.join(supplement_save_path, group, f'{title}_{postfix}_supp.')
             if is_download_supplement:
                 if '' != this_paper['supplemental link'] and os.path.exists(this_paper_main_path) and \
@@ -71,7 +72,7 @@ def download_from_csv(
             if 'error' == this_paper['main link']:
                 error_log.append((title, 'no MAIN link'))
             elif '' != this_paper['main link']:
-                if is_workshops:
+                if is_grouped:
                     os.makedirs(os.path.join(main_save_path, group), exist_ok=True)
                     if is_download_supplement:
                         os.makedirs(os.path.join(supplement_save_path, group), exist_ok=True)
